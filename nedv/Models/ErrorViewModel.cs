4b1059e29086bbf50e6a1c7a279769a1f41b6032
User <User@WIN-SLA7AJNPR4E>
2023-11-22 10:55:15 +0500

    Добавьте файлы проекта.

---
+++ nedv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nedv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
