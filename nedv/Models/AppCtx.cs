b4eee982f0a5cc2dc459a921ab54f881b5c7d3e5
Алексеев Владислав <flazyjob@gmail.com>
2023-11-24 11:54:21 +0500

    Добавление контекста данных, модели User и пакета Tools.

---
+++ nedv/Models/AppCtx.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace nedv.Models
+{
+    public class AppCtx : IdentityDbContext<User>
+    {
+        public AppCtx(DbContextOptions<AppCtx> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ nedv/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace nedv.Models
+{
+    public class User : IdentityUser
+    {   
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+       public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+        public string Surname { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
